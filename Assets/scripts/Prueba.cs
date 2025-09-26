e72c130e60eb7b41d6208ea89545df459419800b
nadhdezz011-alt <nadhdezz011@gmail.com>
2025-09-26 20:54:44 +0100

    add Unity Proyect
    se ha añadido el proyecto de Unity a GitHub

---
+++ Assets/scripts/Prueba.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Prueba : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hola Mundo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
