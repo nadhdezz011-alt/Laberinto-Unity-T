e925e73cc73bd1329f5494864a1bd16600fa9418
nadhdezz011-alt <nadhdezz011@gmail.com>
2025-09-29 20:32:41 +0100

    Add movimiento en awsd
    se añadieron códigos para mover el jugador en las teclas awsd en vertical y horizontal

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public float movimientoEjeX;
-    public float movimientoEjey;
-    public float movimientoEjez;
+    public float movimientoEjeY;
+    public float movimientoEjeZ;
+    public float velocidad = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +15,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(movimientoEjeX, movimientoEjey, movimientoEjez);
+        //Input es una clase que permite obtener la entrada del usuario (teclado, mouse, joystick, etc) 
+        //GetAxis es un metodo que permite obtener el valor de un eje (Horizontal, Vertical, etc)
+        //Time.deltaTime es el tiempo que ha pasado desde el ultimo frame (se usa para que el movimiento sea independiente del frame rate)
+        movimientoEjeZ = Input.GetAxis("Horizontal") * Time.deltaTime;
+        movimientoEjeX = - Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Translate(movimientoEjeX, movimientoEjeY, movimientoEjeZ);
+        //transform es la posicion del objeto en el espacio
+        //translate es para mover el objeto en el espacio
+        //(movimientoEjeX, movimientoEjey, movimientoEjez) son las variables que se usan para mover el objeto en el espacio
+
     }
 }
