4ef49585da5aab7b24ae5cadc7909ce69141cdf2
nadhdezz011-alt <nadhdezz011@gmail.com>
2025-10-01 17:39:40 +0100

    Add ContadorTiempo
    se añadio texto de contador de tiempo al final, jugador velocidad corregido

--- Assets/Scripts/CourseFinish.cs
+++ Assets/Scripts/CourseFinish.cs
@@ -1,10 +1,24 @@
 using UnityEditor;
 using UnityEngine;
+using TMPro;
 
 public class CourseFinish : MonoBehaviour
 {
     [SerializeField]
     GameObject pantallaFinal;
+
+    [SerializeField]
+    TextMeshProUGUI textLabelTime;
+
+    float tiempoDePartida = 0.0f;
+    bool estajugando = true;
+    private void Update()
+    {
+        if (estajugando)
+        {
+            tiempoDePartida = tiempoDePartida + Time.deltaTime;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,8 +27,8 @@ public class CourseFinish : MonoBehaviour
             Debug.Log("Jugador ha llegado a la meta");
             pantallaFinal.SetActive(true);
            other.GetComponent<PlayerMovement>().enabled = false;
-            
-
+            estajugando = false;
+            textLabelTime.text = tiempoDePartida.ToString();
         }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,24 +6,31 @@ public class PlayerMovement : MonoBehaviour
     public float movimientoEjeY;
     public float movimientoEjeZ;
     public float velocidad = 200f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
-        
+       
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        //Input es una clase que permite obtener la entrada del usuario (teclado, mouse, joystick, etc) 
-        //GetAxis es un metodo que permite obtener el valor de un eje (Horizontal, Vertical, etc)
-        //Time.deltaTime es el tiempo que ha pasado desde el ultimo frame (se usa para que el movimiento sea independiente del frame rate)
-        movimientoEjeZ = Input.GetAxis("Horizontal") * Time.deltaTime;
-        movimientoEjeX = - Input.GetAxis("Vertical") * Time.deltaTime;
-        transform.Translate(movimientoEjeX, movimientoEjeY, movimientoEjeZ);
-        //transform es la posicion del objeto en el espacio
-        //translate es para mover el objeto en el espacio
-        //(movimientoEjeX, movimientoEjey, movimientoEjez) son las variables que se usan para mover el objeto en el espacio
+    
+        movimientoEjeZ = Input.GetAxis("Horizontal") * Time.deltaTime * velocidad;
+        movimientoEjeX = - Input.GetAxis("Vertical") * Time.deltaTime * velocidad;
+        transform.Translate(movimientoEjeX, 0, movimientoEjeZ);
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = -Input.GetAxis("Vertical");
+
+        Vector3 direccion = new Vector3(vertical, 0f, horizontal);
+
+        if (direccion.magnitude > 0f)
+        {
+            direccion = direccion.normalized;
+        }
+
+        transform.Translate(direccion * velocidad * Time.deltaTime);
 
     }
 }
