84af0716f1f32d8e7d78bd54b08ac234394055df
nadhdezz011-alt <nadhdezz011@gmail.com>
2025-10-02 09:36:08 +0100

    add canvas with screen
    para que el canvas no se deforme con la pantalla

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,12 +4,12 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform jugador;
     public Vector3 offset = new Vector3(0, 10, -10);
-
+   public Vector2 rotacion = new Vector2(45, 0);
     void LateUpdate()
     {
         if (jugador != null)
         {
-            transform.position = jugador.position + offset;
+            transform.position = jugador.position + offset + new Vector3(0, rotacion.x, 0);
             transform.LookAt(jugador);
         }
     }
