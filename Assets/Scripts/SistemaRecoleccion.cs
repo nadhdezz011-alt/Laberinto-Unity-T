70c491e75bfce6f0e3cf66d6400ce1a55a4fc62f
nadhdezz011-alt <nadhdezz011@gmail.com>
2025-10-03 12:22:41 +0100

    correccion

--- Assets/Scripts/SistemaRecoleccion.cs
+++ Assets/Scripts/SistemaRecoleccion.cs
@@ -5,7 +5,7 @@ public class SistemaRecoleccion : MonoBehaviour
     public int cantidadMonedas = 0;
     public TextMeshProUGUI numero;
     public TextMeshProUGUI numeroFinal;
-    private void Update()
+    private void Start()
     {
         numero.text = cantidadMonedas.ToString();
         numeroFinal.text = cantidadMonedas.ToString();
@@ -14,9 +14,12 @@ public class SistemaRecoleccion : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Moneda"))
-            {
+        {
             Destroy(other.gameObject);
             cantidadMonedas++;
+            numero.text = cantidadMonedas.ToString();
+            numeroFinal.text = cantidadMonedas.ToString();
         }
+
     }
 }
